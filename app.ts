3246e7aa44e9f69633fda17360e4be73dea4c4b2
Naim <naimxf@gmail.com>
2026-01-05 20:55:49 +0600

    init: betterauth and authentication management
    Signed-off-by: Naim <naimxf@gmail.com>

--- app.ts
+++ app.ts
@@ -1,9 +1,13 @@
+import { toNodeHandler } from "better-auth/node";
 import express from "express";
+import { auth } from "./lib/auth";
 import postRouter from "./modules/post/post.routes";
 
 const app = express();
 
 app.use(express.json());
+
+app.all("/api/auth/*splat", toNodeHandler(auth));
 
 app.use("/posts", postRouter);
 
